05a3adddfd7a8173bb3e755d49744ed8ea21c03c
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2023-05-24 18:39:03 +0200

    Adicionafo mais linhas no ex 10_3

--- Cap10/js/ex10_3.js
+++ Cap10/js/ex10_3.js
@@ -10,7 +10,7 @@ frm.addEventListener("submit", (e) => {
     const genero = frm.inGenero.value
 
     adicionarFilme(titulo, genero)
-
+    gravarLista(titulo, genero)
     
     frm.reset()
     frm.inTitulo.focus()
@@ -27,11 +27,40 @@ const adicionarFilme = (titulo, genero) => {
     
     col1.innerText = titulo
     col2.innerText = genero
-    col3.innerHTML = "<i class=`excluir` title=`Excluir`>&#10008</i>"
+    col3.innerHTML = "<i class=exclui title=Excluir>&#10008</i>" //Class exclui esta no CSS
 }
 
 function gravarLista(titulo, genero){
 
+    if(localStorage.getItem("listaFilmes")){
+        
+        //pega as informações do local Storage e já acrescenta as novas, separando com ;
+        let tempFilmes = localStorage.getItem("listaFilmes") + ";" + titulo
+        let tempGenero = localStorage.getItem("listaGenero") + ";" + genero
 
+        //agora salva de volta a variavel atualizada
+        localStorage.setItem("listaFilmes", tempFilmes)
+        localStorage.setItem("listaGenero", tempGenero)    
+    }
+
+    else{
+        //se não existir a variavel ainda, então criamos ela direto
+        localStorage.setItem("listaFilmes", titulo)
+        localStorage.setItem("listaGenero", genero)
+    }
 
 }
+
+window.addEventListener("load", () => {
+    if(localStorage.getItem("listaFilmes")){
+        
+        let filmes = localStorage.getItem("listaFilmes").split(";")
+        let genero = localStorage.getItem("listaGenero").split(";")
+
+        for (const i in filmes) {
+            adicionarFilme(filmes[i], genero[i])
+        }
+    }
+})
+
+
