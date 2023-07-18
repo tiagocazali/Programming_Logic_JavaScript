ce0ae8494fd937eb60164c8f8a77c03cd6aaa996
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2023-07-19 00:38:49 +0200

    inicio da criação do APP.JS

--- Cap12/app.js
+++ Cap12/app.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
   res.send('Hello World! - Seja Bem vindo!')
 })
 
+app.get('/cap12', (req, res) => {
+  res.send('<h2>Capítulo 12: Introdução ao NODE Express</h2>')
+})
+
+//para reconhecer os dados recebidos como sendo um objeto no formato JSON
+app.use(express.json());  //Uso de Middleware, SEMPRE ANTES DE UMA ARROW FUNCTION
+app.post('/filmes', (req, res) => {
+  const {titulo, genero} = req.body; //Usado Desestruturação de dados - Pega o Array e separa em 2 variáveis
+  res.send(`Filme: ${titulo} Genero: ${genero}, recebido...`)
+})
+
+
+const log = (req, res, next) => {
+  console.log(`....... Acessado em ${new Date()}`);
+  console.log('Gravado no servidor essa info!')
+  next();
+}
+
+app.get('/transfer', log, (req, res) => {
+  res.send("Ok! Valor transferido com sucesso!")
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
